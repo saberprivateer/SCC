f99b4eaa4a2a1c1a85d8492199dda44a548f7603
saberprivateer <daniel.kamerling@gmail.com>
2015-07-29 00:38:09 -0700

    shenanigans

--- src/com/com/company/Main.java
+++ src/com/com/company/Main.java
@@ -12,6 +12,47 @@ public class Main {
 
     public static int t = 0;
 
+    public static void howfar(int[] exp) {
+        double explored = 0;
+        double percentexp;
+        for (int anExp : exp) {
+            if (anExp > 0) {
+                explored++;
+            }
+        }
+        percentexp = (explored / exp.length * 100);
+        log(String.format("%.2f", percentexp) + "% loop completed. " + (int) explored + "/" + exp.length);
+
+    }
+
+    public static void preprocess(ArrayList<int[]> g, ArrayList<int[]> grev) {
+        int[] args = new int[g.size()];
+        int[] linked = new int[g.size()];
+        for (int i = 0; i < g.size(); i++) {
+            args[i] = g.get(i)[1];
+            linked[i] = g.get(i)[0];
+        }
+        partitionlinked(args, 0, args.length, linked);
+        for (int i = 0; i < g.size(); i++) {
+            grev.add(i, new int[]{linked[i], args[i]});
+//            grev.get(i)[1] = args[i];
+//            grev.get(i)[0] = linked[i];
+        }
+    }
+
+    public static void preprocesssecond(ArrayList<int[]> g, ArrayList<int[]> gsort) {
+        int[] args = new int[g.size()];
+        int[] linked = new int[g.size()];
+        for (int i = 0; i < g.size(); i++) {
+            args[i] = g.get(i)[0];
+            linked[i] = g.get(i)[1];
+        }
+        partitionlinked(args, 0, args.length, linked);
+        for (int i = 0; i < g.size(); i++) {
+            gsort.add(i, new int[]{args[i],linked[i]});
+        }
+    }
+
     public static void dfsloop(ArrayList<int[]> g, int[] scc) {
         log("Start dfs loop");
         int s;
@@ -26,45 +67,46 @@ public class Main {
         Arrays.fill(exp, -1);
         Arrays.fill(finish, -1);
 
-        double explored;
-        double percentexp;
+        ArrayList<int[]> grev = new ArrayList<>();
+        ArrayList<int[]> gsort = new ArrayList<>();
+        log("grev initial size:" + grev.size());
+        preprocess(g, grev);
+        log("preprocessing comlete - " + grev.get(grev.size() - 1)[1]);
+
+        System.out.println("grev:");
+        for (int[] aG : grev) {
+            System.out.println(Arrays.toString(aG));
+        }
 
         //First pass
         log("START FIRST PASS");
         for (int i = n; i > 0; i--) {
-            explored = 0;
-            for (int anExp : exp) {
-                if (anExp > 0) {
-                    explored++;
-                }
-            }
-            percentexp = (explored/exp.length*100);
-            log(String.format("%.2f" ,percentexp)+"% loop completed. "+(int) explored+"/"+exp.length);
-
+//            howfar(exp);
             if (exp[i - 1] < 0) {
                 s = i;
                 //System.out.println(Arrays.toString(exp)+" and i="+i);
-                dfs(g, i, s, exp, finish, "rev");
+                dfs(grev, i, s, exp, finish, "rev");
             }
         }
 
+        log("");
+        preprocesssecond(g, gsort);
+        System.out.println(Arrays.toString(finish));
+        System.out.println("g:");
+        for (int[] aG : gsort) {
+            System.out.println(Arrays.toString(aG));
+        }
         //Second pass
         log("START SECOND PASS");
         Arrays.fill(exp, -1);
         int fstart;
         int j;
         for (int i = n; i > 0; i--) {
-            explored = 0;
-            for (int anExp : exp) {
-                if (anExp > 0) {
-                    explored++;
-                }
-            }
-            percentexp = explored/exp.length*100;
-            log(String.format("%.2f",percentexp)+"% loop completed. "+(int) explored+"/"+exp.length);
+
+//            howfar(exp);
 
             //log("i is currently "+i);
-            j=0;
+            j = 0;
             while (finish[j] != i) {
                 //log("finish[j]="+finish[j]+" and i="+i);
                 j++;
@@ -74,27 +116,27 @@ public class Main {
                 s = fstart;
                 //System.out.println(Arrays.toString(exp)+" and i="+i);
                 //log("Start dfs from outerloop");
-                dfs(g, fstart, s, exp, finish, "fwd");
+                dfs(gsort, fstart, s, exp, finish, "fwd");
             }
         }
-        countscc(exp,scc);
-        //System.out.println(Arrays.toString(finish));
+        countscc(exp, scc);
+        System.out.println(Arrays.toString(exp));
     }
 
-    public static void countscc(int[] exp,int[] scc) {
+    public static void countscc(int[] exp, int[] scc) {
         //System.out.println(Arrays.toString(exp));
         int[] count = new int[exp.length];
-        for (int i=0;i<count.length;i++){
-            count[exp[i]-1]++;
+        for (int i = 0; i < count.length; i++) {
+            count[exp[i] - 1]++;
         }
         //System.out.println(Arrays.toString(count));
         partition(count, "mot", 0, count.length);
         //System.out.println(Arrays.toString(count));
-        scc[0]=count[count.length-1];
-        scc[1]=count[count.length-2];
-        scc[2]=count[count.length-3];
-        scc[3]=count[count.length-4];
-        scc[4]=count[count.length-5];
+        scc[0] = count[count.length - 1];
+        scc[1] = count[count.length - 2];
+        scc[2] = count[count.length - 3];
+        scc[3] = count[count.length - 4];
+        scc[4] = count[count.length - 5];
         //System.out.println("SCC="+Arrays.toString(scc));
 
 
@@ -102,11 +144,11 @@ public class Main {
 
 
     public static void dfs(ArrayList<int[]> g, int i, int s, int[] exp, int[] finish, String dir) {
-        //log("Start dfs with i=" + i + " and s=" + s);
+        log("Start dfs with i=" + i + " and s=" + s);
         exp[i - 1] = s;
         int first;
         int last;
-
+        int len = g.size();
         //System.out.println(Arrays.toString(exp));
         if (dir.equals("rev")) {
             first = 1;
@@ -118,23 +160,41 @@ public class Main {
 
         int head;
 
-        for (int j = 0; j < g.size(); j++) {
-            if (g.get(j)[first] == i) {
-                head = g.get(j)[last];
+        int locate = 0;
+        while (g.get(locate)[first] != i) {
+            locate++;
+            if (locate >= len) {
+                break;
+            }
+        }
+        log("locate = " + locate);
+        int ch;
+        if (locate < len) {
+            while (g.get(locate)[first] == i) {
+//        for (int j = locate; j < g.size(); j++) {
+                ch = g.get(locate)[first];
+                log("loop is searching for " + i + "==" + ch);
+//            if (g.get(j)[first] == i) {
+                head = g.get(locate)[last];
                 //log("head="+head);
                 //log("The vector "+g.get(j)[0]+"->"+head);
                 if (exp[head - 1] < 0) {
                     //log("recurse on dfs " + head);
                     dfs(g, head, s, exp, finish, dir);
                 }
-                if (j > g.size()) {
+//
+                locate++;
+                if (locate >= len) {
                     break;
                 }
             }
         }
+
+
         Main.t++;
-        if(dir.equals("rev")){
-        finish[i - 1] = Main.t;}
+        if (dir.equals("rev")) {
+            finish[i - 1] = Main.t;
+        }
         /*
         if (dir.equals("rev")) {
             //System.out.println("Finish = " + Arrays.toString(finish));
@@ -153,11 +213,11 @@ public class Main {
         parsedata(edges, datafile);
 
         long endparse = System.nanoTime();
-        long parseduration = (endparse-startparse);
-        log("Parse ran for "+parseduration/1000000+" milliseconds");
+        long parseduration = (endparse - startparse);
+        log("Parse ran for " + parseduration / 1000000 + " milliseconds");
 
-        Main.t=0;
-        dfsloop(edges,answer);
+        Main.t = 0;
+        dfsloop(edges, answer);
         System.out.println("The answer to " + datafile + " is " + Arrays.toString(answer));
     }
 
@@ -166,12 +226,12 @@ public class Main {
 
         log("Begin Program");
         int[] answer = new int[5];
-        testcase("SCC", answer);
+        testcase("testcase3", answer);
         log("End Program");
 
         long endTime = System.nanoTime();
         long duration = (endTime - startTime);
-        log("Program ran for "+duration/1000000+" milliseconds");
+        log("Program ran for " + duration / 1000000 + " milliseconds");
     }
 
     public static void parsedata(ArrayList<int[]> edges, String datafile) throws IOException {
@@ -222,6 +282,7 @@ public class Main {
 
         return arr;
     }
+
     public static void partition(int[] args, String choose, int l, int r) {
         int p;
         int i;
@@ -242,9 +303,9 @@ public class Main {
                 case "mot":
                     //find the middle
                     if (((r - l) & 1) == 0) {
-                        mid = l + ((r-l)/2) - 1;
+                        mid = l + ((r - l) / 2) - 1;
                     } else {
-                        mid = l + (r - l - 1)/2;
+                        mid = l + (r - l - 1) / 2;
                     }
                     if (args[l] > args[mid]) {
                         if (args[mid] > args[r - 1]) {
@@ -296,4 +357,43 @@ public class Main {
         }
 
     }
+
+    public static void partitionlinked(int[] args, int l, int r, int[] linked) {
+        int p;
+        int i;
+        int temp;
+
+        if (r - l > 1) {
+            p = args[l];
+
+            i = l + 1;
+            for (int j = i; j < r; j++) {
+                if (args[j] < p) {
+                    temp = args[j];
+                    args[j] = args[i];
+                    args[i] = temp;
+
+                    temp = linked[j];
+                    linked[j] = linked[i];
+                    linked[i] = temp;
+
+                    i++;
+                }
+            }
+            temp = args[i - 1];
+            args[i - 1] = args[l];
+            args[l] = temp;
+
+            temp = linked[i - 1];
+            linked[i - 1] = linked[l];
+            linked[l] = temp;
+
+            //Partition on the left
+            partitionlinked(args, l, i - 1, linked);
+            //Partition on the right
+            partitionlinked(args, i, r, linked);
+        }
+    }
+
 }
+
