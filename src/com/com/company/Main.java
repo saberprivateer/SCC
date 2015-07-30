b393d50a4785f84e086048ad42a4ee08e667a15e
saberprivateer <daniel.kamerling@gmail.com>
2015-07-30 08:43:59 -0700

    WORKS!!!!!

--- src/com/com/company/Main.java
+++ src/com/com/company/Main.java
@@ -6,6 +6,8 @@ import java.util.Arrays;
 
 public class Main {
 
+    public static int N = 875714;
+
     public static void log(Object args) {
         System.out.println(args.toString());
     }
@@ -25,7 +27,7 @@ public class Main {
 
     }
 
-    public static void dfsloop(ArrayList<int[]> g, int[] scc, int n, ArrayList<Integer> order) {
+    public static void dfsloop(ArrayList<Integer>[] g, int[] scc, int n, ArrayList<Integer> order) {
         Main.t = 0;
         log("Start dfs loop size " + n);
         int s;
@@ -39,7 +41,7 @@ public class Main {
 
         //First pass
         for (int i = n; i > 0; i--) {
-            howfar(exp);
+//            howfar(exp);
             convert = order.indexOf(i) + 1;
             if (exp[convert - 1] < 0) {
                 s = convert;
@@ -71,94 +73,54 @@ public class Main {
         scc[3] = count[count.length - 4];
         scc[4] = count[count.length - 5];
         System.out.println("SCC=" + Arrays.toString(scc));
-
-
     }
 
 
-    public static void dfs(ArrayList<int[]> g, int i, int s, int[] exp, int[] finish) {
-        //log("Start dfs with i=" + i + " and s=" + s);
-        //mark i as explored
-        //also counts as setting leader(i) = s
+    public static void dfs(ArrayList<Integer>[] g, int i, int s, int[] exp, int[] finish) {
         exp[i - 1] = s;
+//        int[] loop = new int[2];
+//        log(g[i-1]);
+//        loop[0] = g[i - 1].get(0);
+//        loop[1] = g[i - 1].get(1);
 
-        int[] loop;
-//        log("loop should be: "+ Arrays.toString(g.get(i-1)));
-        loop = g.get(i - 1);
-//        log("Loop = "+Arrays.toString(loop));
-
-        for (int j = 1; j < loop.length; j++) {
-//            log(j+" check exp "+Arrays.toString(exp)+" and loop[j] = "+loop[j]);
-            if (exp[loop[j] - 1] < 0) {
-                dfs(g, loop[j], s, exp, finish);
+        for (int j = 0; j < g[i-1].size(); j++) {
+            if (exp[g[i-1].get(j) - 1] < 0) {
+                dfs(g, g[i-1].get(j), s, exp, finish);
             }
         }
         Main.t++;
         finish[i - 1] = Main.t;
-        //log("end of dfs");
     }
 
     public static void testcase(String datafile, int[] answer) throws IOException {
-        ArrayList<int[]> edges = new ArrayList<>();
-        ArrayList<int[]> edgesfwd = new ArrayList<>();
-        ArrayList<int[]> edgesrev = new ArrayList<>();
-        ArrayList<int[]> adj = new ArrayList<>();
-        ArrayList<int[]> adjrev = new ArrayList<>();
+        ArrayList<Integer>[] adj = new ArrayList[N];
+        ArrayList<Integer>[] adjrev = new ArrayList[N];
         ArrayList<Integer> order = new ArrayList<>();
         long startparse = System.nanoTime();
 
-        parsedata(edges, datafile);
-        int n = maxnodes(edges);
-        for (int i = 0; i < n; i++) {
+        for (int i=0; i<N; i++){
+            adj[i] = new ArrayList<Integer>(1);
+            adjrev[i] = new ArrayList<Integer>(1);
+        }
+
+        parsedata(datafile, adj, adjrev);
+//        int n = maxnodes(edges);
+
+        for (int i = 0; i < N; i++) {
             order.add(i, i + 1);
         }
         log("");
-//        log("Edges after parse");
-//        for (int i = 0; i < edges.size(); i++) {
-//            log(Arrays.toString(edges.get(i)));
-//        }
-
-        log("begin preprocess, rev");
-        preprocess(edges, edgesrev, "rev");
-//        log("Check edges rev");
-//        for (int i = 0; i < edgesrev.size(); i++) {
-//            log(Arrays.toString(edgesrev.get(i)));
-//        }
-
-//        log("Check edgesfwd");
-        preprocesssecond(edges, edgesfwd);
-//        log("preprocess edgesfwd over");
-//        for (int i = 0; i < edgesfwd.size(); i++) {
-//            log(Arrays.toString(edgesfwd.get(i)));
-//        }
-
-        log("Total number of nodes: " + n);
-        log("");
-        log("Forward Map");
-        convertToAdjacency(edgesfwd, adj, "fwd");
-        log("Reverse Map");
-        convertToAdjacency(edgesrev, adjrev, "rev");
-        log("#nodes = " + n);
-//        log("Adjacency Rev:");
-//        for (int i = 0; i < adjrev.size(); i++) {
-//            log(Arrays.toString(adjrev.get(i)));
-//        }
-//
-//        log("Adjacency:");
-//        for (int i = 0; i < adj.size(); i++) {
-//            log(Arrays.toString(adj.get(i)));
-//        }
-
+        log("#nodes = " + N);
         long endparse = System.nanoTime();
         long parseduration = (endparse - startparse);
-        log("Pre-processing (extraction, parsing, conversion) ran for " + parseduration / 1000000000 + " seconds");
+        log("Pre-processing (extraction and parsing) ran for " + parseduration / 1000000000 + " seconds");
 
         log("START FIRST PASS");
-        dfsloop(adjrev, answer, n, order);
+        dfsloop(adjrev, answer, N, order);
 //        log("Finishing Times:");
 //        log(Arrays.toString(order.toArray()));
         log("START SECOND PASS");
-        dfsloop(adj, answer, n, order);
+        dfsloop(adj, answer, N, order);
 //      System.out.println("The answer to " + datafile + " is " + Arrays.toString(answer));
     }
 
@@ -242,7 +204,7 @@ public class Main {
         log("Adjacency ran for " + duration / 1000000000 + " seconds");
     }
 
-    public static void parsedata(ArrayList<int[]> edges, String datafile) throws IOException {
+    public static void parsedata(String datafile, ArrayList<Integer>[] adj, ArrayList<Integer>[] adjrev) throws IOException {
         long startTime = System.nanoTime();
         log("Begin Parse Data");
         String[] arr = data(datafile);
@@ -254,7 +216,8 @@ public class Main {
             for (int j = 0; j < temp.length; j++) {
                 temparr[j] = Integer.valueOf(temp[j]);
             }
-            edges.add(temparr);
+            adj[temparr[0]-1].add(temparr[1]);
+            adjrev[temparr[1]-1].add(temparr[0]);
         }
         long endTime = System.nanoTime();
         long duration = (endTime - startTime);
@@ -368,80 +331,5 @@ public class Main {
             //Partition on the right
             partition(args, choose, i, r);
         }
-
     }
-
-    public static void partitionlinked(int[] args, int l, int r, int[] linked) {
-//        log("Begin Partition Linked");
-        int p;
-        int i;
-        int temp;
-
-        if (r - l > 1) {
-            p = args[l];
-
-            i = l + 1;
-            for (int j = i; j < r; j++) {
-                if (args[j] < p) {
-                    temp = args[j];
-                    args[j] = args[i];
-                    args[i] = temp;
-
-                    temp = linked[j];
-                    linked[j] = linked[i];
-                    linked[i] = temp;
-
-                    i++;
-                }
-            }
-            temp = args[i - 1];
-            args[i - 1] = args[l];
-            args[l] = temp;
-
-            temp = linked[i - 1];
-            linked[i - 1] = linked[l];
-            linked[l] = temp;
-
-            //Partition on the left
-            partitionlinked(args, l, i - 1, linked);
-            //Partition on the right
-            partitionlinked(args, i, r, linked);
-        }
-    }
-
-    public static void preprocess(ArrayList<int[]> g, ArrayList<int[]> grev, String dir) {
-        int[] args = new int[g.size()];
-        int[] linked = new int[g.size()];
-        int first = 0;
-        int last = 1;
-        if (dir.equals("rev")){
-            first = 1;
-            last = 0;
-        }
-        log("split arrays");
-        for (int i = 0; i < g.size(); i++) {
-            args[i] = g.get(i)[first];
-            linked[i] = g.get(i)[last];
-        }
-        partitionlinked(args, 0, args.length, linked);
-        for (int i = 0; i < g.size(); i++) {
-//            log("[" + linked[i] + "," + args[i] + "]");
-            grev.add(i, new int[]{linked[i], args[i]});
-//            grev.get(i)[1] = args[i];
-//            grev.get(i)[0] = linked[i];
-        }
-    }
-    public static void preprocesssecond(ArrayList<int[]> g, ArrayList<int[]> gsort) {
-        int[] args = new int[g.size()];
-        int[] linked = new int[g.size()];
-        for (int i = 0; i < g.size(); i++) {
-            args[i] = g.get(i)[0];
-            linked[i] = g.get(i)[1];
-        }
-        partitionlinked(args, 0, args.length, linked);
-        for (int i = 0; i < g.size(); i++) {
-            gsort.add(i, new int[]{args[i], linked[i]});
-        }
-    }
-
 }
